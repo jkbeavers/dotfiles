ac6e0b601174a726655ee4e30f863a3c46e1cddf
Justin Beavers <jk.beavers@yahoo.com>
2016-06-21 12:34:29 -0700

    made gitTmux script modular

--- scripts/gitTmuxBar.sh
+++ scripts/gitTmuxBar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## Justin Beavers
 
 ###############################################
 ## This script checks the output of git status 
@@ -6,41 +7,40 @@
 ## untracked file to the right bar in tmux.
 ###############################################
 
-repo1=/home/beavers/Documents/cpp/giordanocpp/								# cpp computational physics repo
-repo2=/home/beavers/dotfiles/												# dotfiles repo
+repo1=/home/beavers/Documents/cpp/giordanocpp/												# cpp computational physics repo
+repo2=/home/beavers/dotfiles/																# dotfiles repo
 
-function pushout ()								# assigns values to push variable if push is needed
+# returns push string if git push is needed
+function pushout ()																			
 {
-	if [ -n "$1" ] 
+	local pushit=$(git log origin/master..master)											# non-empty/empty if remote does/doesn't have commits that are on local 
+
+	if [ -n "$pushit" ] 
 	then
 	    local out='PUSH!'
 	else 
 	    local out=""
 	fi
-echo "$out"
+	echo "$out"
+}
+
+# returns numbers of files/changes to be added to stage/commited and untracked files ($1-$2)
+function fileNums ()																		
+{
+	local changes=$(git status | grep "new file:" | wc -l)									# print number of new files not commited	
+	local modified=$(git status | grep "modified:" | wc -l)									# print number of files not staged for commit:	
+	local untracked=$(git status | grep -A1000 Untracked | tail -n +4 | sed \$d | wc -l)	# print number of files not tracked
+	
+	echo "$(($changes + $modified))-"$untracked"" 
 }
 
 cd "$repo1"
-changes1=$(git status | grep "new file:" | wc -l)								# print number of new file not commited
-
-modified1=$(git status | grep "modified:" | wc -l)								# print number of files not staged for commit:
-
-untracked1=$(git status | grep -A1000 Untracked | tail -n +4 | sed \$d | wc -l)	# print number of files not tracked
-
-pushit1=$(git log origin/master..master) 					# null if remote does not have commits that are on local 
-pushit1=$(pushout "$pushit1")
-
+pushit1=$(pushout)
+files1=$(fileNums)
 
 cd "$repo2"
-changes2=$(git status | grep "new file:" | wc -l)								# print number of new file not commited
+pushit2=$(pushout)
+files2=$(fileNums)
 
-modified2=$(git status | grep "modified:" | wc -l)								# print number of files not staged for commit:
-
-untracked2=$(git status | grep -A1000 Untracked | tail -n +4 | sed \$d | wc -l)	# print number of files not tracked
-
-pushit2=$(git log origin/master..master) 					# null if remote does not have commits that are on local 
-pushit2=$(pushout "$pushit2")
-
-
-tmuxOut=" $pushit1 cpp $modified1-$changes1-$untracked1 $pushit2 dot $modified2-$changes2-$untracked2"
+tmuxOut=" $pushit1 cpp $files1 $pushit2 dot $files2"
 echo $tmuxOut
